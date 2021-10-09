05ed0b0849cf64c3d427323a6c35a40d8952e8d7
Korostast <Korostast@gmail.com>
2021-10-09 12:41:29 +0300

    New defines for code readability

--- parsers.c
+++ parsers.c
@@ -15,17 +15,17 @@ void inputDataParse(const char *currentString, Data *data) {
     strcpy(data->curDayStr[3].word[0], date);
 
     // Обработка температур (ночью - днем - по ощущениям)
-    int countTempNight = sscanf(tempNight, "%d..%d", &data->curDayNums[0], &data->curDayNums[1]);
+    int countTempNight = sscanf(tempNight, "%d..%d", &data->curDayNums[LOW_NIGHT_TEMP], &data->curDayNums[HIGH_NIGHT_TEMP]);
     if (countTempNight == 1)    // If there is only one number in column, then maximum = minimum
-        data->curDayNums[1] = data->curDayNums[0];
+        data->curDayNums[HIGH_NIGHT_TEMP] = data->curDayNums[LOW_NIGHT_TEMP];
 
-    int countTempDay = sscanf(tempDay, "%d..%d", &data->curDayNums[2], &data->curDayNums[3]);
+    int countTempDay = sscanf(tempDay, "%d..%d", &data->curDayNums[LOW_DAY_TEMP], &data->curDayNums[HIGH_DAY_TEMP]);
     if (countTempDay == 1)
-        data->curDayNums[3] = data->curDayNums[2];
+        data->curDayNums[HIGH_DAY_TEMP] = data->curDayNums[LOW_DAY_TEMP];
 
-    int countTempSense = sscanf(tempSense, "%d..%d", &data->curDayNums[4], &data->curDayNums[5]);
+    int countTempSense = sscanf(tempSense, "%d..%d", &data->curDayNums[LOW_SENSE_TEMP], &data->curDayNums[HIGH_SENSE_TEMP]);
     if (countTempSense == 1)
-        data->curDayNums[5] = data->curDayNums[4];
+        data->curDayNums[HIGH_SENSE_TEMP] = data->curDayNums[LOW_SENSE_TEMP];
 
 
     // Обработка осдадков
@@ -36,9 +36,9 @@ void inputDataParse(const char *currentString, Data *data) {
         data->curDayStr[0].size = 0;
 
     // Обработка направлений ветра
-    int countSpeed = sscanf(speed, "%d-%d", &data->curDayNums[6], &data->curDayNums[7]);
+    int countSpeed = sscanf(speed, "%d-%d", &data->curDayNums[LOW_SPEED_WIND], &data->curDayNums[HIGH_SPEED_WIND]);
     if (countSpeed == 1)
-        data->curDayNums[7] = data->curDayNums[6];
+        data->curDayNums[HIGH_SPEED_WIND] = data->curDayNums[LOW_SPEED_WIND];
 
     data->curDayStr[1].size = sscanf(direction, "%[^,],%[^,],%[^,],%[^,],%[^,],%[^,]", data->curDayStr[1].word[0], data->curDayStr[1].word[1],
                                      data->curDayStr[1].word[2], data->curDayStr[1].word[3], data->curDayStr[1].word[4], data->curDayStr[1].word[5]);
@@ -92,12 +92,12 @@ void inputDataParse(const char *currentString, Data *data) {
     }
 
     // Обработка порывов ветра
-    int countGusts = sscanf(gusts, "%d-%d", &data->curDayNums[8], &data->curDayNums[9]);
+    int countGusts = sscanf(gusts, "%d-%d", &data->curDayNums[LOW_SPEED_GUST], &data->curDayNums[HIGH_SPEED_GUST]);
     if (countGusts == 1)
-        data->curDayNums[9] = data->curDayNums[8];
+        data->curDayNums[HIGH_SPEED_GUST] = data->curDayNums[LOW_SPEED_GUST];
 
     // Обработка давления
-    sscanf(pressure, "%d", &data->curDayNums[10]);
+    sscanf(pressure, "%d", &data->curDayNums[VALUE_PRESSURE]);
 
     // Обработка явлений
     data->curDayStr[2].size = sscanf(scene, "%[^,],%[^,],%[^,],%[^,],%[^,],%[^,]", data->curDayStr[2].word[0], data->curDayStr[2].word[1],
